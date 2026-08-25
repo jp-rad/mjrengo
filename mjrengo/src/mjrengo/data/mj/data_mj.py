5d0582477d2525f5c01ec84b436f92f658ab4af1
jp-rad <jp-rad@example.com>
2026-08-25 03:40:16 +0000

    feat: Remove unused glyph_table and related imports; update PYTHONPATH for new structure

--- mjrengo/src/mjrengo/data/mj/data_mj.py
+++
@@ -1,6 +0,0 @@
-glyph_table = {
-    "MJ0001": {"ucs": "U+4E00", "rep": "U+4E00", "set": "mj_plus", "active": True},
-    "GJ0431": {"ucs": "U+9AD8", "rep": "U+9AD8", "set": "mj_plus", "active": False},
-    "MJ022336": {"ucs": "U+845B U+E0103", "rep": "U+845B", "set": "mj_plus", "active": True},
-    "MJ022335": {"ucs": "U+845B U+E0102", "rep": "U+845B", "set": "mj_plus", "active": True},
-}
