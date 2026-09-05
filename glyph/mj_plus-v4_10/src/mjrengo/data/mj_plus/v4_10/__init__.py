b28758c748ba9bd0cde16255a5d9c99f91808984
jp-rad <jp-rad@example.com>
2026-09-05 00:00:07 +0000

    Remove PACKAGE definitions from multiple __init__.py files for cleaner module structure

--- glyph/mj_plus-v4_10/src/mjrengo/data/mj_plus/v4_10/__init__.py
+++ glyph/mj_plus-v4_10/src/mjrengo/data/mj_plus/v4_10/__init__.py
@@ -1,3 +1,1 @@
 from .data_mj_plus_v4_10 import VERSION, GLYPH_TABLE
-
-PACKAGES = "mjrengo-data-mj_plus-v4_10"
--- glyph/template-v0_1_0/src/mjrengo/data/template/v0_1_0/__init__.py
+++ glyph/template-v0_1_0/src/mjrengo/data/template/v0_1_0/__init__.py
@@ -1,3 +1,1 @@
 from .data_template_v0_1_0 import VERSION, GLYPH_TABLE
-
-PACKAGES = "mjrengo-data-template-v0_1_0"
